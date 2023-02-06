ff42f7373745e851a91777957aaa7c9a628823c0
Fox Alice <elviratr@yandex.ru>
2023-02-07 00:17:47 +0300

    feat: make user&movie schema

---
+++ models/Movie.js
@@ -0,0 +1,74 @@
+const mongoose = require('mongoose');
+
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    validate: {
+      validator(value) {
+        return validator.isURL(value);
+      },
+      message: 'Необходимо ввести корректную ссылку на изображение',
+    },
+    type: String,
+    required: true,
+  },
+  trailerLink: {
+    validate: {
+      validator(value) {
+        return validator.isURL(value);
+      },
+      message: 'Необходимо ввести корректную ссылку на видео',
+    },
+    type: String,
+    required: true,
+  },
+  thumbnail: {
+    validate: {
+      validator(value) {
+        return validator.isURL(value);
+      },
+      message: 'Необходимо ввести корректную ссылку на изображение',
+    },
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  movieId : {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
+  nameRU : {
+    type: String,
+    required: true,
+  },
+  nameEN: {
+    type: String,
+    required: true,
+  },
+});
+module.exports = mongoose.model('movie', userSchema);
